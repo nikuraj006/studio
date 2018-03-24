60d130ae9c7d61770cccf96d458828511dbb034b
Sushil ranjan <nikuraj006@gmail.com>
2018-03-24 13:15:26 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,56 @@
+<head>
+    <meta charset="utf-8">
+		<meta name="format-detection" content="telephone=no">
+		<link rel="icon" href="images/favicon.ico">
+		<link rel="shortcut icon" href="images/favicon.ico">
+		<link rel="stylesheet" href="css/stuck.css">
+		<link rel="stylesheet" href="css/owl.carousel.css">
+		<link rel="stylesheet" href="css/form.css">
+		<link rel="stylesheet" href="css/touchTouch.css">
+		<link rel="stylesheet" href="css/camera.css">
+		<link rel="stylesheet" href="css/style.css">
+		<script src="js/jquery.js"></script>
+		<script src="js/jquery-migrate-1.1.1.js"></script>
+		<script src="js/script.js"></script>
+		<script src="js/superfish.js"></script>
+		<script src="js/jquery.equalheights.js"></script>
+		<script src="js/jquery.mobilemenu.js"></script>
+		<script src="js/jquery.easing.1.3.js"></script>
+		<script src="js/tmStickUp.js"></script>
+		<script src="js/jquery.ui.totop.js"></script>
+		<script src="js/touchTouch.jquery.js"></script>
+		<script src="js/owl.carousel.js"></script>
+		<script src="js/sForm.js"></script>
+		<script src="js/camera.js"></script>
+		<!--[if (gt IE 9)|!(IE)]><!-->
+		<script src="js/jquery.mobile.customized.min.js"></script>
+		<!--<![endif]-->
+		<script>
+  </head>
+<header>
+			<div id="stuck_container">
+				<div class="container">
+					<div class="row">
+						<div class="grid_12">
+							<h1>
+								<a href="index.html">
+									<img src="images/logo.png" alt="Your Happy Family">
+								</a>
+							</h1>
+							<div class="menu_block">
+								<nav class="horizontal-nav full-width horizontalNav-notprocessed">
+									<ul class="sf-menu">
+										<li class="current"><a href="index.html">About</a></li>
+										<li><a href="index-1.html">Portfolio</a></li>
+										<li><a href="index-2.html">Resume</a></li>
+										<li><a href="index-3.html">Blog</a></li>
+										<li><a href="index-4.html">Contacts</a></li>
+									</ul>
+								</nav>
+								<div class="clear"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
